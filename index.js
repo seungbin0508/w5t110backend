4d3099ec8a64933b880e5e011cde6336102c9095
SeungBinKim <seungbin0508@gmail.com>
2021-07-16 16:11:49 +0900

    Install dotenv and nodemon

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
+
+app.set('port', process.env.PORT)
+
+app.listen(app.get('port') || 3000, () => {
+	console.log(`Server listening on port ${app.get('port')}`)
+})
