787cf26e4d8ab75bf91de9ea398e7811788839e5
SeungBinKim <seungbin0508@gmail.com>
2021-07-16 17:57:31 +0900

    Add a todo in hashed_password property in usrSchema
    Followed by several links to help implementing it

--- models/user.js
+++ models/user.js
@@ -13,6 +13,11 @@ const userSchema = new mongoose.Schema({
 	hashed_password: {
 		type: String,
 		required: true
+		// todo 비밀번호 암호화 필요
+		// https://dlrow.tistory.com/entry/mongoose-password-%EC%95%94%ED%98%B8%ED%99%94
+		// https://www.google.com/search?q=mongoose+password+hashing&oq=mongoose+password+hashing&aqs=chrome..69i57.5903j0j7&sourceid=chrome&ie=UTF-8
+		// https://developer-mac.tistory.com/55
+		// https://coderrocketfuel.com/article/store-passwords-in-mongodb-with-node-js-mongoose-and-bcrypt
 	},
 	phone: {
 		type: String,
