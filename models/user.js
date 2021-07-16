f2c83e17d770dc12157d613262f6d6fa87948f2f
SeungBinKim <seungbin0508@gmail.com>
2021-07-16 21:22:19 +0900

    Update todo

--- models/user.js
+++ models/user.js
@@ -14,11 +14,6 @@ const userSchema = new mongoose.Schema({
 	password: {
 		type: String,
 		required: true
-		// todo 비밀번호 암호화 필요
-		// https://dlrow.tistory.com/entry/mongoose-password-%EC%95%94%ED%98%B8%ED%99%94
-		// https://www.google.com/search?q=mongoose+password+hashing&oq=mongoose+password+hashing&aqs=chrome..69i57.5903j0j7&sourceid=chrome&ie=UTF-8
-		// https://developer-mac.tistory.com/55
-		// https://coderrocketfuel.com/article/store-passwords-in-mongodb-with-node-js-mongoose-and-bcrypt
 	},
 	phone: {
 		type: String,
@@ -48,4 +43,6 @@ userSchema.pre('save', function (next) {
 		})
 	})
 })
+// todo 입력받은 비밀번호 인증 필요
+// https://coderrocketfuel.com/article/store-passwords-in-mongodb-with-node-js-mongoose-and-bcrypt
 export default mongoose.model('User', userSchema)
