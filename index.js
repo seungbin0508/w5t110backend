d1530cfcc647a245fbaa3ff1aff24eec384eedf7
SeungBinKim <seungbin0508@gmail.com>
2021-07-16 16:16:01 +0900

    Update index.js to import main router

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import router from './api/index.js'
 
 dotenv.config()
 
 const app = express()
 
 app.set('port', process.env.PORT)
+
+app.use(router)
 
 app.listen(app.get('port') || 3000, () => {
 	console.log(`Server listening on port ${app.get('port')}`)
