f6be3c8d52fda9ef62d863da5af902a3bf787fdd
SeungBinKim <seungbin0508@gmail.com>
2021-07-17 21:11:28 +0900

    Import User model and Update post and get methods

--- api/users.js
+++ api/users.js
@@ -1,10 +1,11 @@
 import express from 'express'
+import { User } from '../models/index.js'
 
 const router = express.Router()
 
 router.post('/', async (req, res) => {
 	try {
-		// await User.create(req.body)
+		await User.create(req.body)
 		res.sendStatus(201)
 	} catch (err) {
 		console.log(err)
@@ -15,8 +16,8 @@ router.post('/', async (req, res) => {
 router.get('/:userId', async (req, res) => {
 	const { userId } = req.params
 	try {
-		// const user =  await User.findById(userId).select('-password')
-		// res.send(user)
+		const user =  await User.findById(userId).select('-password')
+		res.send(user)
 	} catch (err) {
 		console.error(err)
 		res.send(400)
