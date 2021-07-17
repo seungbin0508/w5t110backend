894807fa0cf6cc60a55dbc3fc5596ce55fe2d692
SeungBinKim <seungbin0508@gmail.com>
2021-07-17 14:07:06 +0900

    Prevent null or invalid type token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,6 +3,10 @@ import jwt from 'jsonwebtoken'
 const auth = (req, res, next) => {
 	const token = req.headers?.authorization?.split('Bearer ')?.[1]
 
+	if (!token) return res.status(401).json({
+		errorMessage: '토큰이 없거나 Bearer 타입이 아닙니다.'
+	})
+
 	try { const decoded = jwt.verify(token, process.env.JWT_SECRET)} catch (e) {
 		console.error(e)
 		next(e)
