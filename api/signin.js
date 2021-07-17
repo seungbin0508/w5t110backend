70880040a25f89dea21ea9ead022a45d22d89104
SeungBinKim <seungbin0508@gmail.com>
2021-07-17 18:29:48 +0900

    Implement sign in

--- api/signin.js
+++ api/signin.js
@@ -4,7 +4,18 @@ import jwt from 'jsonwebtoken'
 const router = expressRouter()
 
 router.post('/', (req, res) => {
-
+	try {
+		// const { email, password } = req.body
+		// const user = User.find({email})
+		// if(user?.comparePassword()) {
+		// const payload = { userId: user._id, name: user.name}
+		// const options = { expiresIn: '5m' }
+		// const token = jwt.sign(payload, process.env.JWT_SECRET, options)
+		// return res.send({ token }) }
+	} catch (err) {
+		console.error(err)
+		res.status(401).json(err)
+	}
 })
 
 export default router
