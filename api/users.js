bff5d8b327f3590dd8cbd6b0a90791dac7689cfd
SeungBinKim <seungbin0508@gmail.com>
2021-07-17 17:09:05 +0900

    Add get method in users api

--- api/users.js
+++ api/users.js
@@ -12,7 +12,16 @@ router.post('/', async (req, res) => {
 	}
 })
 
-router.get('/')
+router.get('/:userId', async (req, res) => {
+	const { userId } = req.params
+	try {
+		// const user =  await User.findById(userId).select('-password')
+		// res.send(user)
+	} catch (err) {
+		console.error(err)
+		res.send(400)
+	}
+})
 
 router.put('/:userId', (req, res) => {
 	const { userId } = req.params
