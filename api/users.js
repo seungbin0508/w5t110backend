41269a5fe63c146714e4ba50860f2b8d990a88a8
SeungBinKim <seungbin0508@gmail.com>
2021-07-17 16:54:46 +0900

    Add post method in users api

--- api/users.js
+++ api/users.js
@@ -2,7 +2,15 @@ import express from 'express'
 
 const router = express.Router()
 
-router.post('/')
+router.post('/', async (req, res) => {
+	try {
+		// await User.create(req.body)
+		res.sendStatus(201)
+	} catch (err) {
+		console.log(err)
+		res.status(400).json({ err })
+	}
+})
 
 router.get('/')
 
