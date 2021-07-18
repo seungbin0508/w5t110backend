3cff8028a1c18ee6467dd9d3b3ac3d1fa98f8fda
seungbin0508 <seungbin0508@gmail.com>
2021-07-18 10:30:04 +0900

    Add import login api router in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import usersRouter from './users.js'
+import loginRouter from './signin.js'
 
 const router = express.Router()
 
 // router.use('/movies', moviesRouter)
 // router.use('/cinemas', cinemasRouter)
 router.use('/users', usersRouter)
+router.use('/login', loginRouter)
 
 router.all('/', (req, res, next) => {
 	console.log('test')
