52f1b176578db1a6e9a6d847a3e02c37359a7d65
seungbin0508 <seungbin0508@gmail.com>
2021-07-18 10:04:43 +0900

    Implement singin

--- api/signin.js
+++ api/signin.js
@@ -1,17 +1,18 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
+import { User } from '../models/index.js'
 
-const router = expressRouter()
+const router = express.Router()
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
 	try {
-		// const { email, password } = req.body
-		// const user = User.find({email})
-		// if(user?.comparePassword()) {
-		// const payload = { userId: user._id, name: user.name}
-		// const options = { expiresIn: '5m' }
-		// const token = jwt.sign(payload, process.env.JWT_SECRET, options)
-		// return res.send({ token }) }
+		const { email, password } = req.body
+		const user = await User.find({email})
+		if(user?.comparePassword(password)) {
+		const payload = { userId: user._id, name: user.name}
+		const options = { expiresIn: '1h' }
+		const token = jwt.sign(payload, process.env.JWT_SECRET, options)
+		return res.send({ token }) }
 	} catch (err) {
 		console.error(err)
 		res.status(401).json(err)
