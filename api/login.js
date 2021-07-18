13a90a7b6e1d72ae78b558d4325317fa4af28e1d
seungbin0508 <seungbin0508@gmail.com>
2021-07-18 10:59:24 +0900

    Ensure to return 400 if no user under given email is found

--- api/login.js
+++ api/login.js
@@ -7,12 +7,17 @@ const router = express.Router()
 router.post('/', async (req, res) => {
 	try {
 		const { email, password } = req.body
-		const user = await User.find({email})
-		if(user?.comparePassword(password)) {
-		const payload = { userId: user._id, name: user.name}
-		const options = { expiresIn: '1h' }
-		const token = jwt.sign(payload, process.env.JWT_SECRET, options)
-		return res.send({ token }) }
+		const user = await User.findOne({ email })
+		console.log(user)
+		if (user?.comparePassword(password)) {
+			const payload = { userId: user._id, name: user.name }
+			const options = { expiresIn: '1h' }
+			const token = jwt.sign(payload, process.env.JWT_SECRET, options)
+			return res.status(201).send({ token })
+		}
+		return res.status(400).json({
+			errorMessage: '잘못된 이메일 혹은 비밀번호입니다.'
+		})
 	} catch (err) {
 		console.error(err)
 		res.status(401).json(err)
