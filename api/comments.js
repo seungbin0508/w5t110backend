42976d3cbf1c28b6953819fd416f77fb57ea268c
awrde <awrde26@gmail.com>
2021-07-19 12:24:20 +0900

    fix router.post, Add auth(middleware)

--- api/comments.js
+++ api/comments.js
@@ -1,23 +1,23 @@
 import express from 'express'
 import { Comment } from '../models/index.js'
+import auth from '../middlewares/auth'
 
 const router = express.Router()
 
 // 관람평 작성 누를 때 (1. 평점(Comment.star) 클릭, 2. 댓글 작성 3. 버튼 클릭 1,2번 순서 바뀔 수 있으나 3번은 고정)
 // 유저 이름 locals에서 찾기(여기서는 어떻게 했는지 모르겠는데 인증된 정보를 통해 userSchema에서 찾으면될듯? 변경될수도)
-router.post('/test', authMiddleware, async (req, res) => {
+router.post('/', auth, async (req, res) => {
   const user = res.locals.user.name
   const comment = new Comment({
     comment: req.body.comment,
     star: req.body.star,
-    name: user
+    userId: user,
   })
-  await comment.create({comment, star, res.locals.user._id})
+  await comment.create({ comment, star, userId })
 })
 
-router.delete('/', async (req, res) => {
+router.put('/', auth, async (req, res) => {})
 
-})
-
+router.delete('/', auth, async (req, res) => {})
 
 export default router
