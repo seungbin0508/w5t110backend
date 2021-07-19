57506ac8e347abbbcaf1bbd9b4df692be4255280
awrde <awrde26@gmail.com>
2021-07-19 10:14:18 +0900

    fix api\comments in multiple declarations in one scope
    const로 comment가 중복 선언 된 것 / 저장 시 받은 값만 저장하도록 수정

--- api/comments.js
+++ api/comments.js
@@ -1,20 +1,15 @@
-const router = express.router()
+import express from 'express'
 
 // 관람평 작성 누를 때 (1. 평점(Comment.star) 클릭, 2. 댓글 작성 3. 버튼 클릭 1,2번 순서 바뀔 수 있으나 3번은 고정)
 // 유저 이름 locals에서 찾기(여기서는 어떻게 했는지 모르겠는데 인증된 정보를 통해 userSchema에서 찾으면될듯?)
 router.post('/test', authMiddleware, async (req, res) => {
-  const { comment, star } = req.body
-  // const user = res.locals.user.name
-  if (comment.length === 0) {
-    res.status(400).send({ errorMessage: '관람평을 입력해주세요.' })
-    return
-  }
+  const user = res.locals.user.name
   const comment = new Comment({
-    comment: comment,
-    star: star,
+    comment: req.body.comment,
+    star: req.body.star,
     name: user,
   })
-  await comment.save()
+  await comment.create({comment, star, res.locals.user._id})
 })
 
 export default router
