dd28abe6bf549cae0cb97fe82cc231fd65e3923b
BJS-kr <hahajeng1234@gmail.com>
2021-07-19 23:57:46 +0900

    홈 화면 title 정보 추가 및 주석 추가

--- api/movies.js
+++ api/movies.js
@@ -6,14 +6,17 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     const [
+        title,
         trailers,
         photos,
         bookRates,
         rates,
         likedUsers
-    ] = Movie.find().select('-_id trailers photos bookRate ratings.rating likedUsers rate').exec((e, values) => {
+        // 메인화면에서 필요한 정보: 영화제목, 트레일러 각 1, 사진 각 1, 예매율, 좋아요한 유져목록,평점
+    ] = Movie.find().select('-_id title trailers photos bookRate likedUsers ratings.rating rate').exec((e, values) => {
         if (e) return res.status(400).send({response:'Failed to get data'})
         return [
+            values.map(value=>value.title),
             values.map(value=>value.trailers[value.trailers.length-1]),
             values.map(value=>value.photos[value.photos.length-1]),
             values.map(value=>value.bookRate),
@@ -21,7 +24,7 @@ router.get('/', (req, res, next) => {
             values.map(value=>value.likedUsers)
         ]
     })
-    res.status(200).json({ response : trailers, photos, bookRates, rates, likedUsers })
+    res.status(200).json({ response : title, trailers, photos, bookRates, rates, likedUsers })
 });
 
 // populate
