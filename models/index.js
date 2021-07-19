fb56287cf73b147f57e4ed0de132129b0e2b173e
seungbin0508 <seungbin0508@gmail.com>
2021-07-19 22:46:15 +0900

    Mongodb user id and password input

--- models/index.js
+++ models/index.js
@@ -4,27 +4,29 @@ import Comment from './comment.js'
 import Movie from './movie.js'
 
 const connect = () => {
-  if (process.env.NODE_ENV !== 'production') {
-    mongoose.set('debug', true)
-  }
+	if (process.env.NODE_ENV !== 'production') {
+		mongoose.set('debug', true)
+	}
 }
 
 mongoose.connect(
-  'mongodb://localhost:27017/admin',
-  {
-    dbName: 'lotte',
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  (err) => {
-    if (err) console.error('몽고디비 연결 오류', err)
-    else console.log('몽고디비 연결 성공')
-  }
+	'mongodb://localhost:27017/admin',
+	{
+		dbName: 'lotte',
+		user: 'sbk',
+		pass: 'hangh@e99',
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	},
+	(err) => {
+		if (err) console.error('몽고디비 연결 오류', err)
+		else console.log('몽고디비 연결 성공')
+	}
 )
 
 mongoose.connection.on('error', (err) => {
-  console.error('몽고디비 연결 에러', err)
-  connect()
+	console.error('몽고디비 연결 에러', err)
+	connect()
 })
 
 connect()
