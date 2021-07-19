64b67d085e82ddd008b2e3fa6be0755bcbfac1e1
seungbin0508 <seungbin0508@gmail.com>
2021-07-19 11:06:26 +0900

    Refactor if statement to reduce complication for further feature implementation

--- api/login.js
+++ api/login.js
@@ -8,16 +8,19 @@ router.post('/', async (req, res) => {
 	try {
 		const { email, password } = req.body
 		const user = await User.findOne({ email })
-		console.log(user)
-		if (user?.comparePassword(password)) {
-			const payload = { userId: user._id, name: user.name }
-			const options = { expiresIn: '1h' }
-			const token = jwt.sign(payload, process.env.JWT_SECRET, options)
-			return res.status(201).send({ token })
+
+		// Check if user exists and input password is correct
+		if (!(user && user.comparePassword(password))) {
+			return res.status(400).json({
+				errorMessage: '잘못된 이메일 혹은 비밀번호입니다.'
+			})
 		}
-		return res.status(400).json({
-			errorMessage: '잘못된 이메일 혹은 비밀번호입니다.'
-		})
+
+		const payload = { userId: user._id, name: user.name }
+		const options = { expiresIn: '1h'}
+		const token = jwt.sign(payload, process.env.JWT_SECRET, options)
+
+		return res.status(201).send({token})
 	} catch (err) {
 		console.error(err)
 		res.status(401).json(err)
