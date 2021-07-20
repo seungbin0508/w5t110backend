f523eba049d0db7aaef14c962b52ee478f46aa13
awrde <awrde26@gmail.com>
2021-07-20 17:50:35 +0900

    Refactor comment (will change)

--- api/comments.js
+++ api/comments.js
@@ -1,32 +1,24 @@
 import express from 'express'
-import Comment from '../models/comment.js'
+// import Comment from '../models/comment.js'
 import auth from '../middlewares/auth.js'
+import movie from '../models/movie.js'
+import Movie from '../models/movie.js'
 
 const router = express.Router()
 
-// 관람평 작성 누를 때 (1. 평점(Comment.star) 클릭, 2. 댓글 작성 3. 버튼 클릭 1,2번 순서 바뀔 수 있으나 3번은 고정)
-// 유저 이름 locals에서 찾기(여기서는 어떻게 했는지 모르겠는데 인증된 정보를 통해 userSchema에서 찾으면될듯? 변경될수도)
 router.post('/', auth, async (req, res) => {
-  
-  const user = res.locals.user._id
-  // const comment = new Comment({
-  //   comment: req.body.comment,
-  //   star: req.body.star,
-  //   userId: user,
-  // })
   try {
-    Movie.findOneAndUpdate(
-      {title:'블랙위도우'},
+   await Movie.findByIdAndUpdate(
+      req.body.movieId,
       {$push:
         {comments:
           {
       comment: req.body.comment,
       star: req.body.star,
-      userId: user,
+      userId: res.locals.user._id,
     }
   }
   })
-    // await comment.save()
     res.status(200).send()
   } catch (err) {
     console.error(err)
@@ -34,12 +26,14 @@ router.post('/', auth, async (req, res) => {
   }
 })
 
-// Todo 아직 미완 body에서 받아올 것과 param에서 받아올 것 
 router.put('/:commentId', auth, async (req, res) => {
   const { commentId } = req.params
-  const { comment, star } = req.body
+  const { comment, star, movieId } = req.body
+  
   try {
-    await Comment.findByIdAndUpdate(commentId, { $set: { star, comment } }) // Todo문법 확인 필요
+    await Movie.updateOne( 
+      {_id: movieId, 'comments._id':commentId}, 
+      {$set:{"comments.$.comment": comment }})
     res.status(200).send()
   } catch (err) {
     console.error(err)
@@ -47,11 +41,14 @@ router.put('/:commentId', auth, async (req, res) => {
   }
 })
 
-// Todo 아직 미완 body에서 받아올 것과 param에서 받아올 것 
 router.delete('/:commentId', auth, async (req, res) => {
   const { commentId } = req.params
   try {
-    await Comment.findByIdAndDelete(commentId)
+    await Movie.findByIdAndUpdate(
+      req.body.movieId,
+      {$pull:{comments:{_id : commentId}}}
+      )
+
     res.status(200).send()
   } catch (err) {
     console.error(err)
