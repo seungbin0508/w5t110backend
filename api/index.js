c3bb5e14a7679a2208cc4b526e3ab6fc9a0f1d93
BJS-kr <hahajeng1234@gmail.com>
2021-07-21 00:51:52 +0900

    api/movies.js 라우터 연결 및 movies/ 라우터 수정

--- api/index.js
+++ api/index.js
@@ -3,10 +3,11 @@ import usersRouter from './users.js'
 import loginRouter from './login.js'
 import commentsRouter from './comments.js'
 import verificationRouter from './verification.js'
+import moviesRouter from './movies.js'
 
 const router = express.Router()
 
-// router.use('/movies', moviesRouter)
+router.use('/movies', moviesRouter)
 // router.use('/cinemas', cinemasRouter)
 router.use('/users', usersRouter)
 router.use('/login', loginRouter)
--- api/movies.js
+++ api/movies.js
@@ -3,31 +3,22 @@ import { Movie } from '../models/index.js';
 
 const router = express.Router();
 
-
-router.get('/', (req, res, next) => {
-    const [
-        _id,
-        title,
-        trailers,
-        photos,
-        bookRates,
-        rates,
-        likedUsers
+router.get('/', async (req, res, next) => {
+    const movies = await Movie.find().select('title trailers photos bookRate likedUsers ratings.rating rate')
         // 메인화면에서 필요한 정보: 영화 _id, 영화제목, 트레일러 각 1, 사진 각 1, 예매율, 좋아요한 유져목록,평점
-    ] = Movie.find().select('_id title trailers photos bookRate likedUsers ratings.rating rate').exec((e, values) => {
-        if (e) return res.status(400).send({response:'Failed to get data'})
-        return [
-            values.map(value=>value._id),
-            values.map(value=>value.title),
-            values.map(value=>value.trailers[value.trailers.length-1]),
-            values.map(value=>value.photos[value.photos.length-1]),
-            values.map(value=>value.bookRate),
-            values.map(value=>value.rate),
-            values.map(value=>value.likedUsers)
+    const [_id, title, trailers, photos, bookRate, rate, likedUsers]= [
+        movies.map(value=>value._id),
+        movies.map(value=>value.title),
+        movies.map(value=>value.trailers?.[value.trailers.length-1]),
+        movies.map(value=>value.photos[value.photos.length-1]),
+        movies.map(value=>value.bookRate),
+        movies.map(value=>value.rate),
+        movies.map(value=>value.likedUsers)
         ]
+        res.status(200).json({ result:_id, title, trailers, photos, bookRate, rate, likedUsers })
     })
-    res.status(200).json({ response : _id, title, trailers, photos, bookRates, rates, likedUsers })
-});
+    
+
 
 // populate
 router.get('/:movieId', (req, res, next) => {
@@ -40,4 +31,4 @@ router.get('/:movieId', (req, res, next) => {
     }
 });
 
-
+export default router
