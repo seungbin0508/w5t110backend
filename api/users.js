8d763502e41279f35d25a6ba21b8ca43799fdce7
seungbin0508 <seungbin0508@gmail.com>
2021-07-20 17:10:08 +0900

    Fix inline css

--- api/users.js
+++ api/users.js
@@ -11,101 +11,79 @@ router.post('/', async (req, res) => {
 		const { _id: userId } = await User.create(req.body)
 
 		const template = `
-<body>
-        <div style=" font-family: -apple-system, BlinkMacSystemFont, "Segoe UI",
-        Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
-        sans-serif;">
-        <div style="display: flex; justify-content: center">
-            <div
-                style="
-                    color: #2D3436;
-                    margin-top: 50px;
-                    margin-bottom: 50px;
-                    display: flex;
-                    justify-content: center;
-                    background-color: #ECF0F1;
-                    padding: 5px 50px 20px 50px;
-                    width: 40%;
-                    border-radius: 10px;
-                "
-            >
-                <div class="sub-container">
-                    <div style="display: flex">
-                        <img
-                            style="width: 150px"
-                            class="image"
-                            src="https://www.dogdrip.net/dvs/c/20/01/29/d63b61ccdd1fbe519c992782a2e1fac1.png"
-                            alt=""
-                        />
-                    </div>
-                    <div
-                        style="
-                            color: #2D3436;
-                            display: flex;
-                            flex-direction: column;
-                        "
-                    >
-                        <h1>이메일 주소 인증</h1>
-                        <link class="content-container" />
-                        <span>저희 서비스를 이용해주셔서 감사합니다 :손인사: </span>
-                        <br />
-                        <span
-                            >고객님, 아래 버튼을 클릭하여 이메일 인증을
-                            완료해주세요.
-                        </span>
-                        <button
-                            style="
-                                margin: 25px 0 25px 0;
-                                width: 120px;
-                                height: 40px;
-                                background-color: rgb(60, 117, 238);
-                                border: none;
-                                border-radius: 5px;
-                                color: white;
-                                font-weight: 600;
-                                font-size: 15px;
-                            "
-                        >
-                            <a
-                                target="_blank"
-                                style="color: #ECF0F1"
-                                href="http://13.209.84.245/verification/${userId}"
-                                >이메일 인증하기</a
-                            >
-                        </button>
-                        <div
-                            style="
-                                color: #2D3436;
-                                display: flex;
-                                align-items: center;
-                                justify-content: space-between;
-                                font-size: 15px;
-                            "
-                        >
-                            <p>가입을 시도하지 않으셨나요?</p>
-                            <a
-                                style="text-decoration: none"
-                                href="https://www.w3schools.com/css/css_howto.asp"
-                                >온라인 고객센터 바로가기</a
-                            >
-                        </div>
-                        <div>
-                            <h2
-                                style="
-                                    font-size: 18px;
-                                    font-weight: 400;
-                                    margin-bottom: 2px;
-                                "
-                            >
-                                - 펭수 드림 -
-                            </h2>
-                            <span>The Future of Penguin Cinema</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </body>	
+<div style=" font-family: -apple-system, BlinkMacSystemFont, "Segoe UI",
+Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+sans-serif;">
+
+<div
+	style="
+		color: #2d3436;
+		display: flex;
+		justify-content: center;
+		background-color: #ecf0f1;
+		height: 390px;
+		width: 500px;
+		border-radius: 10px;
+	"
+>
+	<div class="sub-container">
+		<div>
+			<img
+				style="width: 80px"
+				class="image"
+				src="https://www.dogdrip.net/dvs/c/20/01/29/d63b61ccdd1fbe519c992782a2e1fac1.png"
+				alt=""
+			/>
+		</div>
+		<div style="color: #2d3436">
+			<h1 style="font-size: 20px">이메일 주소 인증</h1>
+			<link style="font-size: 10px" />
+			<span style="font-size: 15px"
+				>저희 서비스를 이용해주셔서 감사합니다 👋
+			</span>
+			<div>
+				<span style="font-size: 15px"
+					>고객님, 아래 버튼을 클릭하여 이메일 인증을
+					완료해주세요.
+				</span>
+			</div>
+
+			<button
+				style="
+					margin: 25px 0 0 0;
+					width: 120px;
+					height: 40px;
+					background-color: rgb(60, 117, 238);
+					border: none;
+					border-radius: 5px;
+					color: white;
+					font-weight: 600;
+					font-size: 15px;
+				"
+			>
+				<a
+					target="_blank"
+					style="color: #ecf0f1; text-decoration: none"
+					href="http://13.209.84.245/verification/${userId}"
+					>이메일 인증하기</a
+				>
+			</button>
+
+			<div style="color: #2d3436; font-size: 15px">
+				<p>가입을 시도하지 않으셨나요?</p>
+				<a
+					style="text-decoration: none"
+					href="https://www.w3schools.com/css/css_howto.asp"
+					>온라인 고객센터 바로가기</a
+				>
+			</div>
+			<div>
+				<span>- The Future of Penguin Cinema -</span>
+			</div>
+		</div>
+	</div>
+</div>
+
 		`
 		const mailOptions = {
 			from: process.env.MAIL_ID,
